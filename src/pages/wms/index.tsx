eebdbca12971c9f57c9c4d94bf0b2466811e23e3
Ronaldo Junior <junhiowr@hotmail.com>
2022-06-30 14:29:17 -0300

    feat: download xlsx

--- src/pages/wms/index.tsx
+++ src/pages/wms/index.tsx
@@ -3,8 +3,6 @@ import { ReactElement } from 'react';
 import Layout from '../../components/Layout';
 
 export default function Index() {
-  const a = 1;
-
   return <Box>wms</Box>;
 }
 Index.getLayout = function getLayout(page: ReactElement) {
