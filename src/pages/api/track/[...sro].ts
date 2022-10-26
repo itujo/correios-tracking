709ceecbd2472100ef779c027fdf31b8368767eb
Ronaldo Junior <junhiowr@hotmail.com>
2022-10-26 16:21:14 -0300

    fix(correios): fix tracking cors

--- src/pages/api/track/[...sro].ts
+++ src/pages/api/track/[...sro].ts
@@ -4,11 +4,19 @@ import {
   RastreioResponse,
 } from 'correios-brasil/dist';
 import { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const { sro }: { sro?: string[] } = req.query;
   let count = -1;
   const response = await Promise.all(
